2275ab5143e3075434a759d7f6a7c3668da9832d
Giuliano Giacaglia <raulzito234@gmail.com>
2015-07-09 13:38:50 -0400

    Initial Commit

---
+++ Spot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spot
+//
+//  Created by Giuliano Giacaglia on 7/9/15.
+//  Copyright (c) 2015 Giuliano Giacaglia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
